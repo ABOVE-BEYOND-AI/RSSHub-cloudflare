a816c385d45a121f65073dde392c5c25094be26e
Ace-Valentine <max@siteshift.co.uk>
2025-09-05 16:33:51 +0100

    workers: export fetch directly from app in _worker.js

--- _worker.js
+++ _worker.js
@@ -2,6 +2,4 @@
 import app from './dist/app.js';
 
 // RSSHub’s export is a Hono app (or a fetch handler). This adapter works for both.
-export default {
-  fetch: (request, env, ctx) => app.fetch(request, env, ctx),
-};
+export default { fetch: app.fetch };
